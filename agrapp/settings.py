c8d78f9d69cab2316730dfb5a1a90bddae3cac76
Mintah Obed Aboah <obed30mintah@yahoo.com>
2020-01-02 09:51:07 -0500

    updated settings

--- agrapp/settings.py
+++ agrapp/settings.py
@@ -35,11 +35,12 @@ ADMINS = (
 ALLOWED_HOSTS = ['127.0.0.1']
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'agrapp.market@gmail.com'
-EMAIL_HOST_PASSWORD = r'@gr@@p123'
+EMAIL_HOST_USER = '' # your email address here
+EMAIL_HOST_PASSWORD = r'' # your password goes here
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL = 'Admin<admin@127.0.0.1:8000>'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # 465
